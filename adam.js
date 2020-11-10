479cf109fdb143b3bb965cc8bcf908714d786ef3
Agannon3 <70546541+Agannon3@users.noreply.github.com>
2020-11-10 14:20:11 -0500

    r1

---
+++ adam.js
@@ -0,0 +1,23 @@
+
+const btn = document.createElement('button');
+
+
+let bta1 = document.querySelector('#bta');
+let bta2 = document.querySelector('#btb');
+let bta3 = document.querySelector('#bta');
+
+bta1.addEventListener('click', () =>{
+	document.body.style.backgroundImage = "url(IMG_3149.jpg)"
+});
+
+bta2.addEventListener('click', () =>{
+	document.body.style.backgroundImage = "url(IMG_3150copy.jpg)"
+});
+
+bta3.addEventListener('click', () =>{
+	document.body.style.background-color == blue;
+});
+
+bta4.addEventListener('click', () =>{
+	document.button.style.background-color == blue;
+});
